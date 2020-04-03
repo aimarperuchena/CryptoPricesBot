018bc263a5ef7008fac850f13059ad2f6aca0561
aperuchena <aimarperuchena@gmail.com>
2020-04-04 00:01:19 +0200

    selenium implementado terminado

--- bot.py
+++ bot.py
@@ -1,22 +1,12 @@
-""" from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
-driver = webdriver.Chrome('chromedriver.exe')
-driver.maximize_window()
-driver.get("https://cryptowat.ch/es-es/")
-html=driver.page_source
-print(html)
-time.sleep(100000)
-driver.close() """
-
-
-
 import urllib.request
-
+import time
 import urlopen
 import request
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+cont = 1
 
 
 def leerDatos(ranking, simbolo, nombre, precio, volumen, capitalizacion, cambio24h, cambio7d, cambio1m, cambio6m, cambio1a, oferta, demanda, ratio):
@@ -39,19 +29,19 @@ def leerDatos(ranking, simbolo, nombre, precio, volumen, capitalizacion, cambio2
 
 def readLine(linea):
     tds = linea.findAll("div")
-    ranking=tds[0].text
-    td_simbolo=tds[1]
+    ranking = tds[0].text
+    td_simbolo = tds[1]
     leerDatos(tds[0].text, tds[1].text, tds[2].text, tds[3].text, tds[4].text, tds[5].text, tds[6].text,
               tds[7].text, tds[8].text, tds[9].text, tds[10].text, tds[11].text, tds[12].text, tds[13].text)
-    
 
 
-def leerHtml():
-    headers = {
+def leerHtml(html):
+    print("LEER HTML")
+    """ headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
     reg_url = 'https://cryptowat.ch/es-es/assets'
     req = Request(url=reg_url, headers=headers)
-    html = urlopen(req).read()
+    html = urlopen(req).read() """
     soup2 = BeautifulSoup(html)
 
     body = soup2.find("div", {"id": "__next"})
@@ -65,5 +55,24 @@ def leerHtml():
         readLine(linea)
 
 
-leerHtml()
-
+def abrirPagina():
+    
+    driver = webdriver.Chrome('chromedriver.exe')
+    driver.maximize_window()
+    driver.get("https://cryptowat.ch/es-es/assets")
+    btnEuro = driver.find_element_by_xpath("//button[@id='eur']")
+    btnEuro.click()
+    for x in range(1, 13):
+        html = driver.page_source
+        leerHtml(html)
+        if x<12:
+            link="https://cryptowat.ch/es-es/assets?page="+str(x+1)
+            btnSiguiente=None
+            while btnSiguiente==None: 
+                try:
+                    btnSiguiente=driver.find_element_by_xpath("//*[contains(text(), 'siguiente')]")
+                except:
+                    print("An exception occurred"+ str(x+1))
+            btnSiguiente.click()        
+    driver.close
+abrirPagina()
